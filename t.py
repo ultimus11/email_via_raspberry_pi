707751843488c7f018f50d5577afa708468c4528
sahil <sahil11panindre@gmail.com>
2019-09-16 14:12:50 +0530

    Add files via upload

---
+++ t.py
@@ -0,0 +1,3 @@
+import rpi_mail_cam_capture
+rpi_mail_cam_capture.send_mail("senderemail@gmail.com","senderpassword","receiveremail@gmail.com","type subject here")
+#before you run the program turn on less secure app access of senders gmail account refer this link "https://myaccount.google.com/lesssecureapps"
